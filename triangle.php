d8f39d2d0fab77b71bfef8aa9f3c58a7d840959b
Vladislavs <Vladikuk948@gmail.com>
2022-09-12 16:40:43 +0300

    text

---
+++ triangle.php
@@ -0,0 +1,16 @@
+<?php
+# функция принимает 3 значения а б с 
+# метод возращает правду если треугольник может создан со сторонами данными длинной
+# и нет если не получается
+# 
+# сделать проверку на 0 и негативные значения
+# и сделать проверку на больше 1
+
+/*
+Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+(In this case, all triangles must have surface greater than 0 to be accepted).
+*/
+
+
+
+?>
