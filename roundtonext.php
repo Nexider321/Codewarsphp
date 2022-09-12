a67c5553af4a946f4005532844fa1ad531c91fa4
Vladislavs <Vladikuk948@gmail.com>
2022-09-12 15:19:17 +0300

    text

---
+++ roundtonext.php
@@ -0,0 +1,26 @@
+<?php
+# Дан тип инт входное надо округлить до 5
+# надо проверить делиться ли число на 5
+# надо прибавить число которое не достаеться
+#
+/*
+Given an integer as input, can you round it to the next 5?
+Examples:
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+etc.
+Input may be any positive or negative integer (including 0).
+You can assume that all inputs are valid integers.
+*/
+
+
+
+
+
+?>
+
