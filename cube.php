7425c48b6a310da66e4414679e999cb0169e7b55
Vladislavs <Vladikuk948@gmail.com>
2022-09-12 21:57:56 +0300

    не понятный алгоритм

---
+++ cube.php
@@ -0,0 +1,16 @@
+<?php
+
+# Надо узнать количество невидимых глазу кубов которые находяться сзади
+# куб состоит из 3 маленьких кубов
+# и нужно сделать проверку что он не меньше этих трех
+# что бы узнать количество кубов
+# нужно применить формулу (n-2)*(n-2)*(n-2)
+
+/* 
+Imagine there's a big cube consisting of n^3 small cubes. Calculate, how many small cubes are not visible from outside.
+For  example, if we have a cube which has 4 cubes in a row, then the function should return 8, because there are 8 cubes inside our cube (2 cubes in each dimension)
+*/ 
+
+
+
+?>
