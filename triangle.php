fcb368bb984565d0ff3914b4f2690b57ce38a6ce
Vladislavs <Vladikuk948@gmail.com>
2022-09-12 16:46:53 +0300

    6work

--- triangle.php
+++ triangle.php
@@ -3,6 +3,7 @@
 # метод возращает правду если треугольник может создан со сторонами данными длинной
 # и нет если не получается
 # 
+#	if ($a <=0 $b <=0 $c <=0){
 # сделать проверку на 0 и негативные значения
 # и сделать проверку на больше 1
 
@@ -11,6 +12,16 @@ Implement a method that accepts 3 integer values a, b, c. The method should retu
 (In this case, all triangles must have surface greater than 0 to be accepted).
 */
 
+
+function triangle($a,$b,$c) {
+		if ($a+$b<=$c || $a+$c<=$b || $b+$c<=$a) {
+		return "false";
+	} else {
+	return "true";
+}
+
+}
+echo triangle(20,20,20);
 
 
 ?>
