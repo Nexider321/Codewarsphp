394398b19d13817d735507ba57151091a4c3a8e7
Vladislavs <Vladikuk948@gmail.com>
2022-09-13 16:16:38 +0300

    text

---
+++ 15multiplication.php
@@ -0,0 +1,21 @@
+<?php
+# Надо создать таблицу умножения которая принимает 2 числа
+# Создаём массив
+# и делаем цикл для создания строчки до того пока не кончиться первая переменная
+# и внутри цикла делаем цикл для создания столбиков до того пока не кончиться вторая переменная
+# и там же умножаем эти два числа и получаем число в середине
+#
+/*
+Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
+Example:
+multiplication_table(3,3)
+1 2 3
+2 4 6
+3 6 9
+-->[[1,2,3],[2,4,6],[3,6,9]]
+Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
+*/
+
+
+?>
+
