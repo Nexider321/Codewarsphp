92bbfcef546db126241ec0ad1ebdfad377a014e9
Vladislavs <Vladikuk948@gmail.com>
2022-09-13 14:41:39 +0300

    text

---
+++ 13iqtest.php
@@ -0,0 +1,17 @@
+<?php
+# даётся массив чисел
+# мы должны разделить его на одиночные цифры
+# мы делим его на два и если он делится без остатка то это чётное число
+# обычно число 1 отличается так что можно сделать свитчи 
+# на переменную в которой будет только 1 значение и оно будет правильное и это мы будем возращать
+/*
+Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that is different in evenness, and return a position of this number.
+! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
+Examples :
+iqTest("2 4 7 8 10") => 3 // Third number is odd, while the rest of the numbers are even
+iqTest("1 2 1 1") => 2 // Second number is even, while the rest of the numbers are odd
+*/
+
+
+
+?>
