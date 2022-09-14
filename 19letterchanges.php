88fe93b083d05e87ed9359b06442d1a882892337
Vladislavs <Vladikuk948@gmail.com>
2022-09-14 21:33:20 +0300

    texgt

---
+++ 19letterchanges.php
@@ -0,0 +1,20 @@
+<?php
+
+# алгоритм шифр цезаря на одну букву в перед
+# с помощью функций ord() chr() 
+# будем просто прибавлять к букве по 1 биту тем самым двигая её в перед когда будем конвертировать в chr()
+/*
+Welcome to this Kata. In this Kata you will be given a string. Your task is to replace every character with the letter following it in the alphabet (for example, "b" should be "c", "z" should be "a" and capital "Z" should be "A").
+The test cases would not have any special symbols or numbers but it will have spaces. And the upper and lower cases should be retained in your output.
+For Example:
+letterChange('Lorem Ipsum')    // return Mpsfn Jqtvn
+*/
+
+function letterChange($word) {
+	$changedWord = ord($word);
+	return chr($changedWord + 1);
+}
+
+letterChange('a');
+
+?>
