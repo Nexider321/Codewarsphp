0c8d985edf845a5cff612591e69fefab0daffc37
Vladislavs <Vladikuk948@gmail.com>
2022-09-14 15:53:45 +0300

    text

---
+++ 18autocomplete.php
@@ -0,0 +1,28 @@
+<?php
+# Функция принимает  текст который надо найти и словарь слов (Массив)
+#  надо сделать Регулярное выражение которые убирает цифры и символы и оставляет только текст
+# создаём пустой массив что бы было что вернуть
+# дальше делаем цикл и проверяем каждое слово в словаре
+# Если есть совпадение типов то пушим в наш массив эти слова
+# и если в массиве меньше 5 слов а если больше то не пушим и возращаем пустой массив
+#
+
+/*
+It's time to create an autocomplete function! Yay!
+The autocomplete function will take in an input string and a dictionary array and return the values from the dictionary that start with the input string. If there are more than 5 matches, restrict your output to the first 5 results. If there are no matches, return an empty array.
+Example:
+autocomplete('ai', ['airplane','airport','apple','ball']) = ['airplane','airport']
+For this kata, the dictionary will always be a valid array of strings. Please return all results in the order given in the dictionary, even if they're not always alphabetical. The search should NOT be case sensitive, but the case of the word should be preserved when it's returned.
+For example, "Apple" and "airport" would both return for an input of 'a'. However, they should return as "Apple" and "airport" in their original cases.
+Important note:
+Any input that is NOT a letter should be treated as if it is not there. For example, an input of "$%^" should be treated as "" and an input of "ab*&1cd" should be treated as "abcd".
+*/
+
+
+function autocomplete()
+
+
+
+?>
+
+
