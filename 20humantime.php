4c656e2780a39310d17d2df7d2409d0b45895daa
Vladislavs <Vladikuk948@gmail.com>
2022-09-15 15:55:41 +0300

    gatavs

--- 20humantime.php
+++ 20humantime.php
@@ -12,7 +12,15 @@ The maximum time never exceeds 359999 (99:59:59)
 You can find some examples in the test fixtures.
 */
 
-
-
+function timeFormat($sec){
+	if ( $sec > 359999){
+	return "Секунды не могут прывышать значение в 359999 \n";
+	} elseif(is_int($sec)){
+	return sprintf('%02d:%02d:%02d', ($sec/ 3600),($sec/ 60 % 60), $sec% 60);	
+	} else{
+	return "Это должно быть числом";
+	}
+}
+echo timeFormat(359999);
 
 ?>
