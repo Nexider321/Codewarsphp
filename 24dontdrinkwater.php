022187077b6fa99c7b84d3ef22f110590a45362b
Vladislavs <Vladikuk948@gmail.com>
2022-09-16 22:00:16 +0300

    text

---
+++ 24dontdrinkwater.php
@@ -0,0 +1,31 @@
+<?php
+# Создать исходный массив
+# создать пустой массив
+# выставить приоритеты O = 1 A = 2 W = 3 H = 4
+# Разделить массив и каждую букву закидывать в 1 2 3 или 4 один массив
+# и вывести массив по порядку 1 2 3 4
+# если в массиве ничего нету то ничего не возращать 
+
+
+/*
+Don't Drink the Water
+Given a two-dimensional array representation of a glass of mixed liquids, sort the array such that the liquids appear in the glass based on their density. (Lower density floats to the top) The width of the glass will not change from top to bottom.
+======================
+|   Density Chart    |
+======================
+| Honey   | H | 1.36 |
+| Water   | W | 1.00 |
+| Alcohol | A | 0.87 |
+| Oil     | O | 0.80 |
+----------------------
+[                            [
+ ['H', 'H', 'W', 'O'],        ['O','O','O','O']
+ ['W', 'W', 'O', 'W'],  =>    ['W','W','W','W']
+ ['H', 'H', 'O', 'O']         ['H','H','H','H']
+ ]                           ]
+The glass representation may be larger or smaller. If a liquid doesn't fill a row, it floats to the top and to the left.
+*/
+
+
+
+?>
