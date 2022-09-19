a7cd03caa6269fb87a7dc6390478282fb4342211
Vladislavs <Vladikuk948@gmail.com>
2022-09-19 22:18:00 +0300

    text

---
+++ 8kyu-removespace.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+/*
+ * Simple, remove the spaces from the string, then return the resultant string.
+ */
+
+function no_space(string $s): string {
+  // Your code here
+ return str_replace(' ', '', $s);
+}
+?>
