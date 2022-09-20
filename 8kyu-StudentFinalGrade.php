d940688c3d4cb62b2ff12b76270fba9d9fbb4a8b
Vladislavs <Vladikuk948@gmail.com>
2022-09-20 13:52:01 +0300

    3work

--- 8kyu-StudentFinalGrade.php
+++ 8kyu-StudentFinalGrade.php
@@ -30,6 +30,21 @@ Examples(Inputs-->Output):
 
 *Use Comparison and Logical Operators.
  */
-
-
+function finalGrade($exam, $projects) {
+// write your answer here
+switch(true){
+  case $exam > 90 || $projects > 10:
+    return "100";
+    break;
+    case $exam > 75 && $projects >= 5:
+    return "90";
+    break;
+    case $exam > 50 && $projects >= 2:
+    return "75";
+    break;
+    default: 
+    return "0";
+}
+  
+}
 ?>
