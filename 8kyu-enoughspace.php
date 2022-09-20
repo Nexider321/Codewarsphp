7d09785ea2599396c6a065ebc8ad6989109a100f
Vladislavs <Vladikuk948@gmail.com>
2022-09-20 13:32:59 +0300

    work

--- 8kyu-enoughspace.php
+++ 8kyu-enoughspace.php
@@ -19,7 +19,15 @@ cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
 
  */
-
+function enough($cap, $on, $wait) {
+  // your code here
+  $sum = ($on + $wait);
+  if ($sum > $cap) {
+    return $sum - $cap;
+  } else{
+      return 0;
+  }
+}
 
 
 
